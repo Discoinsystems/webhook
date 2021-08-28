e586d40a6e2c82325083bad75f0dab855c293453
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-08-28 02:17:51 +0200

    [+] Data for Webhook

---
+++ farming/data-for-webhook.php
@@ -0,0 +1,24 @@
+<?php
+$webhookurl = "https://discoin-webhook-url";
+
+// Grafikkarte
+$graname = "Name der Graka";
+$gramhz = "Hashrate der Graka";
+
+// Host
+$hostname = "Hostname des GrakaHost";
+$hoststate = "Der aktuelle Status";
+$hosloc = "Der Serverstandort";
+
+// Paket
+$pakname = "Name des Paketes";
+$pakpayed = "Nächste Zahlung";
+$pakpayedsince = "Erste Zahlung";
+
+// Discoins
+$farmed = "Ingesamt gefarmt"; //+
+$stromkosten = "Kosten des verbrauchten Stroms"; //-
+$packkosten = "Kosten der Graka"; //-
+
+include "main.php"
+?>
